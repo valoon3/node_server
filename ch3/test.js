903e0304cd45e5bf77b562b3ba5ff7556dcaf16b
valoon3 <valoon2@naver.com>
2022-05-24 22:19:42 +0900

    mysql + sequelize 준비

--- ch3/test.js
+++ ch3/test.js
@@ -4,6 +4,11 @@
 // console.log(global);
 //
 
-console.log(this);
-console.log(this === exports);
-console.log(this === module.exports);
+// console.log(this);
+// console.log(this === exports);
+// console.log(this === module.exports);
+
+const path = require('path');
+const basepath = path.basename(__filename);
+console.log(basepath);
+console.log(path.basename(__dirname));
---
+++ models/index.js
@@ -0,0 +1,19 @@
+'use strict';
+
+const fs = require('fs');
+const path = require('path');
+const Sequelize = require('sequelize');
+const basename = path.basename(__filename);
+
+const env = process.env.NODE_ENV || 'development';
+const config = require(__dirname + '/../config/config.json')[env];
+const db = {};
+
+const sequelize = new Sequelize(config.database, config.username, config.username, config);
+db.sequelize = Sequelize;
+
+
+module.exports = db;
+// sequelize는 시퀄라이즈 패키지이자 생성자
+// config/config.json 에서 데이터베이스 설정을 불러온 후 new Sequelize를 통해 MySQL 연결 객체를 생성한다.
+// 나중에 재사용하기 위해 db.sequelize에 넣어둔다.
