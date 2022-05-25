5c9d115885f289136e79466ce28930ffeeb6fec0
valoon3 <valoon2@naver.com>
2022-05-25 21:25:12 +0900

    일정에도 없는 rebase mysql + sequelize 오류 수정 연결 성공

--- models/index.js
+++ models/index.js
@@ -9,8 +9,8 @@ const env = process.env.NODE_ENV || 'development';
 const config = require(__dirname + '/../config/config.json')[env];
 const db = {};
 
-const sequelize = new Sequelize(config.database, config.username, config.username, config);
-db.sequelize = Sequelize;
+const sequelize = new Sequelize(config.database, config.username, config.password, config);
+db.sequelize = sequelize;
 
 
 module.exports = db;
